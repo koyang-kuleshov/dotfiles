31594607548f04c894567dac6538db33f60590c6
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2021-02-02 00:42:22 +0300

    Added spectacle, set up polybar and i3

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@ echo "***** Update standart applications *****"
 sudo apt update
 sudo apt -y upgrade
 sudo apt install -y python3 python3-venv python3-pudb build-essential libssl-dev libffi-dev python3-dev exuberant-ctags pyflakes vim-gui-common neovim
-sudo apt install -y thunderbird anki gimp telegram-desktop discord doublecmd-common smplayer zathura zathura-djvu zathura-pdf-poppler stow htop tmux tilix mc keepassxc i3
+sudo apt install -y thunderbird anki gimp telegram-desktop discord doublecmd-common smplayer zathura zathura-djvu zathura-pdf-poppler stow htop tmux tilix mc keepassxc i3 kde-spectacle
 
+# install soft
+cd ~
+mkdir soft
+cd soft
+git clone https://github.com/Hskl14/polybar-weather.git
 # echo "***** Install Flatpak applications *****"
 # sudo flatpak install flathub com.slack.Slack
 # sudo flatpak install io.dbeaver.DBeaverCommunity
@@ -21,6 +26,7 @@ stow pudb
 stow tmux
 stow i3
 stow xsession
+stow spectacle
 
 # Google Chrome
 echo "***** Google Chrome Install *****"
