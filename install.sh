61c8c70cf12dc756a8230cdb7898a567344c5b48
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2021-02-04 21:22:07 +0300

    Added polyvar compiling and modify configs of i3, polybar, alacritty

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 echo "***** Update standart applications *****"
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y python3 python3-venv python3-pudb build-essential libssl-dev libffi-dev python3-dev exuberant-ctags pyflakes vim-gui-common neovim
-sudo apt install -y thunderbird anki gimp telegram-desktop discord doublecmd-common smplayer zathura zathura-djvu zathura-pdf-poppler stow htop tmux tilix mc keepassxc i3 j4-dmenu-desktop kde-spectacle pavucontrol pasystray numlockx compton nitrogen alacritty
+sudo apt install -y python3 python3-venv python3-pudb build-essential libssl-dev libffi-dev python3-dev exuberant-ctags pyflakes vim-gui-common neovim git
+sudo apt install -y thunderbird anki gimp telegram-desktop discord doublecmd-common smplayer zathura zathura-djvu zathura-pdf-poppler stow htop tmux mc keepassxc
+sudo apt install -y i3 j4-dmenu-desktop kde-spectacle pavucontrol pasystray numlockx compton nitrogen alacritty
+sudo apt install -y build-essential cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libpulse-dev libjsoncpp-dev i3-wm
+
 
 # install soft
 cd ~
@@ -27,6 +30,8 @@ stow tmux
 stow i3
 stow xsession
 stow dunst
+stow alacritty
+stow zathura
 
 # Google Chrome
 echo "***** Google Chrome Install *****"
@@ -39,8 +44,19 @@ sudo wget http://repo.yandex.ru/yandex-disk/yandex-disk_latest_amd64.deb
 sudo dpkg -i yandex-disk_latest_amd64.deb
 echo "For setup Yandex disk run yandex-disk setup"
 
+
 echo "***** Remove temporary files *****"
 sudo apt -y autoremove
-rm -f YANDEX-DISK-KEY.GPG 
+rm -f YANDEX-DISK-KEY.GPG
 rm -f yandex-disk_latest_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
+
+# Compiling polybar
+git clone --recursive https://github.com/polybar/polybar
+cd polybar
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+# Optional. This will install the polybar executable in /usr/local/bin
+sudo make install
