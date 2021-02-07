521bcdaadfe7fbdcc0791787fe39489355ac8886
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2021-02-07 23:51:22 +0300

    Added laptop configuration and additional apps

--- install-laptop.sh
+++ install-laptop.sh
@@ -4,17 +4,23 @@ sudo apt -y upgrade
 sudo apt install -y python3 python3-venv python3-pudb build-essential libssl-dev libffi-dev python3-dev exuberant-ctags pyflakes vim-gui-common neovim git
 sudo apt install -y thunderbird anki gimp telegram-desktop discord doublecmd-common smplayer zathura zathura-djvu zathura-pdf-poppler stow htop tmux mc keepassxc
 sudo apt install -y i3 j4-dmenu-desktop kde-spectacle pavucontrol pasystray numlockx compton nitrogen alacritty
-sudo apt install -y build-essential cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libpulse-dev libjsoncpp-dev i3-wm
+sudo apt install -y build-essential cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libpulse-dev libjsoncpp-dev i3-wm light
 
+sudo chmod +s /usr/bin/light
+
+# Install siji font
+cd ~
+git clone https://github.com/stark/siji && cd siji
+. install.sh
 
 # install soft
+cd ~
 echo "***** Install Flatpak applications *****"
 flatpak install flathub com.slack.Slack
 flatpak install io.dbeaver.DBeaverCommunity
 flatpak install flathub org.gnome.Fractal
 
 # Stow
-cd ..
 echo "***** Stow settings *****"
 rm ../.bashrc ../.profile
 stow bash
@@ -24,12 +30,14 @@ stow latexmk
 stow nvim
 stow pudb
 stow tmux
-stow i3
 stow xsession
 stow dunst
 stow alacritty
 stow zathura
+cd laptop
+stow i3
 stow polybar
+cd ..
 
 # Google Chrome
 echo "***** Google Chrome Install *****"
