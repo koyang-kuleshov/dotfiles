40902f93e1e7d7c135cfbd3ed14ab91a8a08fde6
Michel Koyang-Kuleshov <michel@koyang-kuleshov.ru>
2021-07-18 09:31:40 +0300

    Version for Pop_OS 21.04

--- install-laptop.sh
+++ install-laptop.sh
@@ -1,26 +1,22 @@
 echo "***** Update standart applications *****"
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y python3 python3-venv python3-pudb build-essential libssl-dev libffi-dev python3-dev exuberant-ctags pyflakes vim-gui-common neovim git ipython jupyter-core python3-pip python3-matplotlib python3-scipy mosh curl wget zip unzip
-sudo apt install -y thunderbird anki gimp telegram-desktop discord doublecmd-common smplayer zathura zathura-djvu zathura-pdf-poppler stow htop tmux mc keepassxc freeplane xcompmgr guake
-sudo apt install -y i3 j4-dmenu-desktop kde-spectacle pavucontrol pasystray numlockx compton nitrogen alacritty
-sudo apt install -y build-essential cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libpulse-dev libjsoncpp-dev i3-wm light
+sudo apt install -y python3 python3-venv python3-pudb build-essential libssl-dev libffi-dev python3-dev exuberant-ctags vim-gui-common neovim git python3-ipython jupyter-core python3-pip python3-matplotlib python3-scipy mosh curl wget zip unzip thunderbird anki gimp telegram-desktop discord doublecmd-common smplayer zathura zathura-djvu zathura-pdf-poppler stow htop tmux mc keepassxc freeplane xcompmgr guake i3 j4-dmenu-desktop kde-spectacle pavucontrol pasystray numlockx compton nitrogen alacritty build-essential cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libpulse-dev libjsoncpp-dev i3-wm light code polybar
 
 sudo chmod +s /usr/bin/light
 xcompmgr -c -l0 -t0 -r0 -o.00
 
 # Install siji font
-cd ~
+cd
 git clone https://github.com/stark/siji && cd siji
 . install.sh
 
 # install soft
-cd ~
+cd
 echo "***** Install Flatpak applications *****"
 flatpak install -y flathub com.slack.Slack
 flatpak install -y io.dbeaver.DBeaverCommunity
 flatpak install -y flathub org.gnome.Fractal
-flatpak install -y flathub com.visualstudio.code
 
 # Stow
 echo "***** Stow settings *****"
@@ -45,11 +41,13 @@ cd ..
 
 # Google Chrome
 echo "***** Google Chrome Install *****"
+cd
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
 
 # Yandex Disk
 echo "***** Yandex Disk Install *****"
+cd
 sudo wget http://repo.yandex.ru/yandex-disk/yandex-disk_latest_amd64.deb
 sudo dpkg -i yandex-disk_latest_amd64.deb
 echo "For setup Yandex disk run yandex-disk setup"
@@ -57,16 +55,6 @@ echo "For setup Yandex disk run yandex-disk setup"
 
 echo "***** Remove temporary files *****"
 sudo apt -y clean
-rm -f YANDEX-DISK-KEY.GPG
+cd
 rm -f yandex-disk_latest_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
-
-# Compiling polybar
-git clone --recursive https://github.com/polybar/polybar
-cd polybar
-mkdir build
-cd build
-cmake ..
-make -j$(nproc)
-# Optional. This will install the polybar executable in /usr/local/bin
-sudo make install
--- polybar-laptop/.config/polybar/launch.sh
+++ polybar-laptop/.config/polybar/launch.sh
@@ -2,12 +2,11 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch bar
 echo "---" | tee -a /tmp/polybar-mainbar.log
-polybar mainbar 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar mainbar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Bar launched..."
